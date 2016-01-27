6e7bf97904a58f74475c3f4066dc3c4733fe5cf6
Gregor Capuder <capuderg@gmail.com>
2016-01-27 14:18:54 +0100

    Initial commit with basic plugin info.

---
+++ one-click-demo-import.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: One Click Demo Import
+Plugin URI: http://www.proteusthemes.com
+Description: WordPress import made easy. Theme authors: Enable simple demo import for your theme demo data.
+Version: 0.1-alpha
+Author: ProteusThemes
+Author URI: http://www.proteusthemes.com
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl.html
+Text Domain: pt-ocdi
+*/
+
