599a8d6f299dd0d9a16bbbf8c337f087bbd2fdf2
Gregor Capuder <capuderg@gmail.com>
2016-01-28 13:25:59 +0100

    Abstracted importer.

---
+++ inc/class-ocdi-importer.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * SINGLETON
+ * Class for declaring the importer used in the One Click Demo Import plugin
+ */
+
+if ( ! class_exists( 'OCDI_Importer' ) ) {
+	class OCDI_Importer {
+
+		private static $instance;
+		private $importer, $importer_options;
+
+		/**
+		 * Private constructor to prevent creating a new instance of the *Singleton* via the `new` operator from outside of this class.
+		 */
+		private function __construct() {
+			// Include files that are needed for WordPress Importer v2
+			$this->include_require_files();
+
+			// Importer options array
+			$this->importer_options = apply_filters( 'pt-ocdi/importer_options', array(
+				'fetch_attachments' => true,
+			) );
+
+			// Set the WordPress Importer v2 as the importer used in this plugin
+			// More: https://github.com/humanmade/WordPress-Importer
+			$this->importer = new WXR_Importer( $this->importer_options );
+		}
+
+		/*
+		 * Include required files
+		 */
+		private function include_require_files() {
+			if ( ! class_exists( 'WP_Importer' ) ) {
+				defined( 'WP_LOAD_IMPORTERS' ) || define( 'WP_LOAD_IMPORTERS', true );
+				require ABSPATH . '/wp-admin/includes/class-wp-importer.php';
+			}
+			require PT_OCDI_PATH . 'vendor/humanmade/WordPress-Importer/class-logger.php';
+			require PT_OCDI_PATH . 'vendor/humanmade/WordPress-Importer/class-logger-cli.php';
+			require PT_OCDI_PATH . 'vendor/humanmade/WordPress-Importer/class-wxr-importer.php';
+		}
+
+		/*
+		 * Static function for retrieving or instantiation of this class - Singleton
+		 */
+		public static function get_instance() {
+			if ( null === static::$instance ) {
+				static::$instance = new static();
+			}
+
+			return static::$instance;
+		}
+
+		/**
+		 * Imports demo data from a WordPress export file
+		 *
+		 * @param xml file, file with WordPress demo export data
+		 */
+		public function import( $data_file ) {
+			$this->importer->import( $data_file );
+		}
+
+		/**
+		 * Set the logger used in the import
+		 *
+		 * @param object logger, logger instance
+		 */
+		public function set_logger( $logger ) {
+			$this->importer->set_logger( $logger );
+		}
+
+		/**
+		 * Private clone method to prevent cloning of the instance of the *Singleton* instance.
+		 *
+		 * @return void
+		 */
+		private function __clone() {}
+
+		/**
+		 * Private unserialize method to prevent unserializing of the *Singleton* instance.
+		 *
+		 * @return void
+		 */
+		private function __wakeup() {}
+
+	}
+}
--- one-click-demo-import.php
+++ one-click-demo-import.php
@@ -23,7 +23,15 @@ define( 'PT_OCDI_VERSION', apply_filters( 'pt-ocdi/version', '1.0.0' ) );
  */
 class PT_One_Click_Demo_Import {
 
+	private $importer;
+
 	function __construct() {
+		// Include files
+		require PT_OCDI_PATH . 'inc/class-ocdi-importer.php';
+
+		// Create importer instance *Singleton*
+		$this->importer = OCDI_Importer::get_instance();
+
 		// Actions
 		add_action( 'admin_menu', array( $this, 'create_plugin_page' ) );
 	}
@@ -45,10 +53,40 @@ class PT_One_Click_Demo_Import {
 	function display_plugin_page() {
 	?>
 		<div class="wrap">
-			<h2><span class="dashicons dashicons-download" style="line-height: 29px;"></span> Import Demo Data</h2>
+			<h2><span class="dashicons dashicons-download" style="line-height: 30px;"></span> One Click Demo Import</h2>
+
+			<form method="post" class="js-one-click-import-form">
+				<input type="hidden" name="demononce" value="<?php echo wp_create_nonce('radium-demo-code'); ?>" />
+				<input name="reset" class="panel-save button-primary" type="submit" value="Import Demo Data" />
+				<input type="hidden" name="action" value="demo-data" />
+			</form>
+
+			<script>
+				jQuery( function ( $ ) {
+					'use strict';
+					$( '.js-one-click-import-form' ).on( 'submit', function () {
+						$( this ).append( '<p style="font-width: bold; font-size: 1.5em;"><span class="spinner" style="display: inline-block; float: none; visibility: visible;"></span> Importing now, please wait!</p>' );
+						$( this ).find( '.panel-save' ).attr( 'disabled', true );
+					} );
+				} );
+			</script>
+
 		</div>
 	<?php
+
+		$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+
+		if( 'demo-data' == $action && check_admin_referer('radium-demo-code' , 'demononce')){
+
+			$file = PT_OCDI_PATH ."demo-import-files/demo-import-post-page-image.xml";
+			echo $file . "<br><br>";
+
+			$this->importer->set_logger( new WP_Importer_Logger_CLI() );
+			$this->importer->import( $file );
+
+		}
 	}
+
 }
 
 new PT_One_Click_Demo_Import();
