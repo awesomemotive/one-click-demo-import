5eb1fc08ad2392617005606e2f5225f78e662f32
Gregor Capuder <capuderg@gmail.com>
2016-02-04 12:54:23 +0100

    Changed importer to a regular class (not singleton anymore). Filter now working. Added select for demo data files.

--- inc/class-ocdi-importer.php
+++ inc/class-ocdi-importer.php
@@ -1,35 +1,25 @@
 <?php
 
 /*
- * SINGLETON
  * Class for declaring the importer used in the One Click Demo Import plugin
  */
 
 if ( ! class_exists( 'OCDI_Importer' ) ) {
 	class OCDI_Importer {
 
-		private static $instance;
-		private $importer, $importer_options, $logger;
+		private $importer, $logger;
 
-		/**
-		 * Private constructor to prevent creating a new instance of the *Singleton* via the `new` operator from outside of this class.
-		 */
-		private function __construct() {
+		function __construct( $importer_options, $logger_min_level ) {
 			// Include files that are needed for WordPress Importer v2
 			$this->include_require_files();
 
-			// Importer options array
-			$this->importer_options = apply_filters( 'pt-ocdi/importer_options', array(
-				'fetch_attachments' => true,
-			) );
-
 			// Set the WordPress Importer v2 as the importer used in this plugin
 			// More: https://github.com/humanmade/WordPress-Importer
-			$this->importer = new WXR_Importer( $this->importer_options );
+			$this->importer = new WXR_Importer( $importer_options );
 
 			// Set the default logger
-			$this->logger = new OCDI_Logger();
-			$this->logger->min_level = apply_filters( 'pt-ocdi/logger_min_level', 'debug' );
+			$this->logger            = new OCDI_Logger();
+			$this->logger->min_level = $logger_min_level;
 			$this->importer->set_logger( $this->logger );
 		}
 
@@ -45,21 +35,10 @@ if ( ! class_exists( 'OCDI_Importer' ) ) {
 			require PT_OCDI_PATH . 'vendor/humanmade/WordPress-Importer/class-wxr-importer.php';
 		}
 
-		/*
-		 * Static function for retrieving or instantiation of this class - Singleton
-		 */
-		public static function get_instance() {
-			if ( null === static::$instance ) {
-				static::$instance = new static();
-			}
-
-			return static::$instance;
-		}
-
 		/**
 		 * Imports demo data from a WordPress export file
 		 *
-		 * @param xml file, file with WordPress demo export data
+		 * @param $data_file, path to xml file, file with WordPress demo export data
 		 */
 		public function import( $data_file ) {
 			$this->importer->import( $data_file );
@@ -68,25 +47,11 @@ if ( ! class_exists( 'OCDI_Importer' ) ) {
 		/**
 		 * Set the logger used in the import
 		 *
-		 * @param object logger, logger instance
+		 * @param $logger, object, logger instance
 		 */
 		public function set_logger( $logger ) {
 			$this->importer->set_logger( $logger );
 		}
-
-		/**
-		 * Private clone method to prevent cloning of the instance of the *Singleton* instance.
-		 *
-		 * @return void
-		 */
-		private function __clone() {}
-
-		/**
-		 * Private unserialize method to prevent unserializing of the *Singleton* instance.
-		 *
-		 * @return void
-		 */
-		private function __wakeup() {}
 
 	}
 }
--- one-click-demo-import.php
+++ one-click-demo-import.php
@@ -23,26 +23,22 @@ define( 'PT_OCDI_VERSION', apply_filters( 'pt-ocdi/version', '0.1-alpha' ) );
  */
 class PT_One_Click_Demo_Import {
 
-	private $importer, $plugin_page;
+	private $importer, $plugin_page, $importer_options, $logger_min_level, $import_files;
 
 	function __construct() {
 		// Include files
 		require PT_OCDI_PATH . 'inc/class-ocdi-importer.php';
 
-		// Create importer instance *Singleton*
-		$this->importer = OCDI_Importer::get_instance();
-
 		// Actions
 		add_action( 'admin_menu', array( $this, 'create_plugin_page' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
-		add_action( 'wp_ajax_ocdi_import_data', array( $this, 'ocdi_import_data_callback' ) );
+		add_action( 'wp_ajax_ocdi_import_data', array( $this, 'import_data_ajax_callback' ) );
+		add_action( 'after_setup_theme', array( $this, 'setup_plugin_with_filter_data' ) );
 	}
 
 
 	/**
 	 * Creates the plugin page and a submenu item in WP Appearance menu
-	 *
-	 * @since 1.0.0
 	 */
 	function create_plugin_page() {
 		$this->plugin_page = add_theme_page( 'One Click Demo Import', 'Import Demo Data', 'switch_themes', 'pt-one-click-demo-import', array( $this, 'display_plugin_page' ) );
@@ -51,16 +47,36 @@ class PT_One_Click_Demo_Import {
 
 	/**
 	 * Plugin page display
-	 *
-	 * @since 1.0.0
 	 */
 	function display_plugin_page() {
 	?>
 		<div class="wrap  ocdi">
-			<h2 class="ocdi__title"><span class="dashicons  dashicons-download"></span> One Click Demo Import</h2>
-			<p>TODO: General description of demo import goes here...</p>
+			<h2 class="ocdi__title"><span class="dashicons  dashicons-download"></span><?php esc_html_e( 'One Click Demo Import', 'pt-ocdi' ); ?></h2>
+			<p>
+				<?php esc_html_e( 'TODO: General description of demo import goes here...', 'pt-ocdi' ); ?>
+			</p>
 
-			<button class="panel-save  button-primary  js-ocdi-import-data  ocdi__button">Import Demo Data</button>
+			<p>
+				<?php if ( empty( $this->import_files ) ) : ?>
+					<div class="error  below-h2">
+						<p>
+							<?php esc_html_e( 'There are no import files available!', 'pt-ocdi' ); ?>
+						</p>
+					</div>
+				<?php else : ?>
+				<select id="demo-import-files">
+					<?php foreach ( $this->import_files as $import_file ) : ?>
+						<option value="<?php echo esc_url( $import_file['import_file_url'] ); ?>">
+							<?php echo esc_html( $import_file['import_file_name'] ); ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			<?php endif; ?>
+			</p>
+
+			<p>
+				<button class="panel-save  button-primary  js-ocdi-import-data  ocdi__button" <?php disabled( true, empty( $this->import_files ) ); ?>><?php esc_html_e( 'Import Demo Data', 'pt-ocdi' ); ?></button>
+			</p>
 
 			<div class="js-ocdi-ajax-response  ocdi__response"></div>
 		</div>
@@ -71,7 +87,7 @@ class PT_One_Click_Demo_Import {
 	/**
 	 * Enqueue admin scripts (JS and CSS)
 	 *
-	 * @since 1.0.0
+	 * @param $hook, holds info on which admin page you are currently looking at
 	 */
 	function admin_enqueue_scripts( $hook ) {
 		// enqueue the scripts only on the plugin page
@@ -80,9 +96,9 @@ class PT_One_Click_Demo_Import {
 
 			wp_localize_script( 'ocdi-main-js', 'ocdi',
 				array(
-					'ajax_url'   => admin_url( 'admin-ajax.php' ),
-					'ajax_nonce' => wp_create_nonce( 'ocdi-ajax-verification' ),
-					'file'       => PT_OCDI_PATH . 'demo-import-files/demo-import-post-page-image.xml'
+					'ajax_url'     => admin_url( 'admin-ajax.php' ),
+					'ajax_nonce'   => wp_create_nonce( 'ocdi-ajax-verification' ),
+					'file'         => PT_OCDI_PATH . 'demo-import-files/demo-import-post-page-image.xml'
 				)
 			);
 
@@ -92,11 +108,9 @@ class PT_One_Click_Demo_Import {
 
 
 	/**
-	 * AJAX callback function
-	 *
-	 * @since 1.0.0
+	 * AJAX import callback function
 	 */
-	function ocdi_import_data_callback() {
+	function import_data_ajax_callback() {
 		check_ajax_referer( 'ocdi-ajax-verification', 'security' );
 
 		$file = $_POST['file'];
@@ -106,6 +120,26 @@ class PT_One_Click_Demo_Import {
 		$this->importer->import( $file );
 
 		wp_die();
+	}
+
+
+	/**
+	 * Get data from filters, after the theme has loaded and instantiate the importer
+	 */
+	function setup_plugin_with_filter_data() {
+		// Get info of import data files
+		$this->import_files = apply_filters( 'pt-ocdi/import_files', array() );
+
+		// Importer options array
+		$this->importer_options = apply_filters( 'pt-ocdi/importer_options', array(
+			'fetch_attachments' => true,
+		) );
+
+		// Logger reporting level for the importer
+		$this->logger_min_level = apply_filters( 'pt-ocdi/logger_min_level', 'notice' );
+
+		// Create importer instance with proper parameters
+		$this->importer = new OCDI_Importer( $this->importer_options, $this->logger_min_level );
 	}
 
 }
