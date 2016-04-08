9ff12e58ca5b5606fd6032a08bcae98cff024e7a
Gregor Capuder <capuderg@gmail.com>
2016-04-08 15:39:30 +0200

    Added some more tests.

--- tests/test-ocdi-helpers.php
+++ tests/test-ocdi-helpers.php
@@ -8,7 +8,109 @@ class OCDIHelpersTest extends WP_UnitTestCase {
 	function test_helper_validate_import_file_info() {
 
 		// Test empty array input
-		$this->assertEquals( array(), OCDI_Helpers::validate_import_file_info( array() ) );
+		$import_files    = array();
+		$expected_output = array();
+		$this->assertEquals( $expected_output, OCDI_Helpers::validate_import_file_info( $import_files ) );
+
+		// Test valid array
+		$import_files = array(
+			array(
+				'import_file_name'       => 'Demo Import 1',
+				'import_file_url'        => 'http://www.your_domain.com/ocdi/demo-content.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets.json'
+			),
+			array(
+				'import_file_name'       => 'Demo Import 2',
+				'import_file_url'        => 'http://www.your_domain.com/ocdi/demo-content2.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets2.json'
+			),
+		);
+		$expected_output = array(
+			array(
+				'import_file_name'       => 'Demo Import 1',
+				'import_file_url'        => 'http://www.your_domain.com/ocdi/demo-content.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets.json'
+			),
+			array(
+				'import_file_name'       => 'Demo Import 2',
+				'import_file_url'        => 'http://www.your_domain.com/ocdi/demo-content2.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets2.json'
+			),
+		);
+		$this->assertEquals( $expected_output, OCDI_Helpers::validate_import_file_info( $import_files ) );
+
+		// Test valid array with one invalid item
+		$import_files = array(
+			array(
+				'import_file_name'       => 'Demo Import 1',
+				'import_file_url'        => 'http://www.your_domain.com/ocdi/demo-content.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets.json'
+			),
+			array(
+				'import_file_title'      => 'Invalid Demo import',
+				'import_file_link'       => 'http://www.your_domain.com/ocdi/invalid-demo-content.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/invalid-widgets.json'
+			),
+			array(
+				'import_file_name' => 'Demo Import 2',
+				'import_file_url'  => 'http://www.your_domain.com/ocdi/demo-content2.xml',
+			),
+		);
+		$expected_output = array(
+			array(
+				'import_file_name'       => 'Demo Import 1',
+				'import_file_url'        => 'http://www.your_domain.com/ocdi/demo-content.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/widgets.json'
+			),
+			array(
+				'import_file_name' => 'Demo Import 2',
+				'import_file_url'  => 'http://www.your_domain.com/ocdi/demo-content2.xml',
+			),
+		);
+		$this->assertEquals( $expected_output, OCDI_Helpers::validate_import_file_info( $import_files ) );
+
+		// Test invalid array
+		$import_files = array(
+			array(
+				'import_file_title'      => 'Invalid Demo import',
+				'import_file_link'       => 'http://www.your_domain.com/ocdi/invalid-demo-content.xml',
+				'import_widget_file_url' => 'http://www.your_domain.com/ocdi/invalid-widgets.json'
+			),
+			array(
+				'import_file_title' => 'Invalid Demo import',
+				'import_file_url'   => 'http://www.your_domain.com/ocdi/invalid-demo-content.xml',
+			),
+			array(
+				'import_file_name' => 'Invalid Demo import',
+				'import_file_link' => 'http://www.your_domain.com/ocdi/invalid-demo-content.xml',
+			)
+		);
+		$expected_output = array();
+		$this->assertEquals( $expected_output, OCDI_Helpers::validate_import_file_info( $import_files ) );
+	}
+
+	function test_helper_download_import_files() {
+
+		// Test with empty file url. Should return WP_Error obj.
+		$import_files = array(
+			'import_file_name' => 'Import file',
+			'import_file_url'  => '',
+		);
+		$expected_output = new WP_Error(
+			'url_not_defined',
+			'URL for <strong>Import file</strong> file is not defined!'
+		);
+		$this->assertEquals( $expected_output , OCDI_Helpers::download_import_files( $import_files ) );
+
+		// Test with invalid url. Should return WP_Error obj.
+		$import_files = array(
+			'import_file_name' => 'Import file',
+			'import_file_url'  => 'http://www.yourinvaliddomain.com/ocdi/invalid-demo-content.xml',
+		);
+		$expected_output = new WP_Error(
+			'file_fetching_error',
+			'An error occurred while fetching <strong>Import file</strong> file from the server!<br>Reason: http_request_failed - Couldn\'t resolve host \'www.yourinvaliddomain.com\'.<br>'
+		);
+		$this->assertEquals( $expected_output , OCDI_Helpers::download_import_files( $import_files ) );
 	}
 }
-
