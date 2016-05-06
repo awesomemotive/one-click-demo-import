6b79ad25f882c5ae616cb23aa32c5100a8d1a392
Gregor Capuder <capuderg@gmail.com>
2016-05-06 13:43:24 +0200

    Extend the WXR_Importer class to get/set the protected variables in the class.

---
+++ inc/class-ocdi-wxr-importer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * WXR importer class used in the One Click Demo Import plugin.
+ * Needed to extend the WXR_Importer class to get/set the importer protected variables,
+ * for use in the multiple AJAX calls.
+ *
+ * @package ocdi
+ */
+
+// Include required files.
+require PT_OCDI_PATH . 'vendor/humanmade/WordPress-Importer/class-wxr-importer.php';
+
+class OCDI_WXR_Importer extends WXR_Importer {
+
+	/**
+	 * Get all protected variables from the WXR_Importer needed for continuing the import.
+	 */
+	public function get_importer_data() {
+		return array(
+			'mapping'            => $this->mapping,
+			'requires_remapping' => $this->requires_remapping,
+			'exists'             => $this->exists,
+			'user_slug_override' => $this->user_slug_override,
+			'url_remap'          => $this->url_remap,
+			'featured_images'    => $this->featured_images,
+		);
+	}
+
+	/**
+	 * Sets all protected variables from the WXR_Importer needed for continuing the import.
+	 *
+	 * @param array $data with set variables.
+	 */
+	public function set_importer_data( $data ) {
+		$this->mapping            = empty( $data['mapping'] ) ? array() : $data['mapping'];
+		$this->requires_remapping = empty( $data['requires_remapping'] ) ? array() : $data['requires_remapping'];
+		$this->exists             = empty( $data['exists'] ) ? array() : $data['exists'];
+		$this->user_slug_override = empty( $data['user_slug_override'] ) ? array() : $data['user_slug_override'];
+		$this->url_remap          = empty( $data['url_remap'] ) ? array() : $data['url_remap'];
+		$this->featured_images    = empty( $data['featured_images'] ) ? array() : $data['featured_images'];
+	}
+}
