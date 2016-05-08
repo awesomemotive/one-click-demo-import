071be0b41d172630ab6073a82e600d45e5f96701
Gregor Capuder <capuderg@gmail.com>
2016-05-08 12:40:45 +0200

    Fixed menu items and tags importing issue with multiple AJAX calls.

--- inc/class-ocdi-wxr-importer.php
+++ inc/class-ocdi-wxr-importer.php
@@ -4,6 +4,9 @@
  * Needed to extend the WXR_Importer class to get/set the importer protected variables,
  * for use in the multiple AJAX calls.
  *
+ * Overwritten process_post method to fix the menu items/tags import issue. This
+ * should be removed when it is fixed in the original importer.
+ *
  * @package ocdi
  */
 
@@ -38,5 +41,236 @@ class OCDI_WXR_Importer extends WXR_Importer {
 		$this->user_slug_override = empty( $data['user_slug_override'] ) ? array() : $data['user_slug_override'];
 		$this->url_remap          = empty( $data['url_remap'] ) ? array() : $data['url_remap'];
 		$this->featured_images    = empty( $data['featured_images'] ) ? array() : $data['featured_images'];
+	}
+
+
+
+	/**
+	 * Create new posts based on import information
+	 *
+	 * Posts marked as having a parent which doesn't exist will become top level items.
+	 * Doesn't create a new post if: the post type doesn't exist, the given post ID
+	 * is already noted as imported or a post with the same title and date already exists.
+	 * Note that new/updated terms, comments and meta are imported for the last of the above.
+	 *
+	 *
+	 * The only thing that is changed in this function is a typecesting to int!!!
+	 * It is marked with a couple of lines of comments -> !!!OCDI:
+	 *
+	 */
+	protected function process_post( $data, $meta, $comments, $terms ) {
+		/**
+		 * Pre-process post data.
+		 *
+		 * @param array $data Post data. (Return empty to skip.)
+		 * @param array $meta Meta data.
+		 * @param array $comments Comments on the post.
+		 * @param array $terms Terms on the post.
+		 */
+		$data = apply_filters( 'wxr_importer.pre_process.post', $data, $meta, $comments, $terms );
+		if ( empty( $data ) ) {
+			return false;
+		}
+
+		$original_id = isset( $data['post_id'] )     ? (int) $data['post_id']     : 0;
+		$parent_id   = isset( $data['post_parent'] ) ? (int) $data['post_parent'] : 0;
+		$author_id   = isset( $data['post_author'] ) ? (int) $data['post_author'] : 0;
+
+		// Have we already processed this?
+		if ( isset( $this->mapping['post'][ $original_id ] ) ) {
+			return;
+		}
+
+		$post_type_object = get_post_type_object( $data['post_type'] );
+
+		// Is this type even valid?
+		if ( ! $post_type_object ) {
+			$this->logger->warning( sprintf(
+				__( 'Failed to import "%s": Invalid post type %s', 'wordpress-importer' ),
+				$data['post_title'],
+				$data['post_type']
+			) );
+			return false;
+		}
+
+		$post_exists = $this->post_exists( $data );
+		if ( $post_exists ) {
+			$this->logger->info( sprintf(
+				__('%s "%s" already exists.', 'wordpress-importer'),
+				$post_type_object->labels->singular_name,
+				$data['post_title']
+			) );
+
+			return false;
+		}
+
+		// Map the parent post, or mark it as one we need to fix
+		$requires_remapping = false;
+		if ( $parent_id ) {
+			if ( isset( $this->mapping['post'][ $parent_id ] ) ) {
+				$data['post_parent'] = $this->mapping['post'][ $parent_id ];
+			} else {
+				$meta[] = array( 'key' => '_wxr_import_parent', 'value' => $parent_id );
+				$requires_remapping = true;
+
+				$data['post_parent'] = 0;
+			}
+		}
+
+		// Map the author, or mark it as one we need to fix
+		$author = sanitize_user( $data['post_author'], true );
+		if ( empty( $author ) ) {
+			// Missing or invalid author, use default if available.
+			$data['post_author'] = $this->options['default_author'];
+		} elseif ( isset( $this->mapping['user_slug'][ $author ] ) ) {
+			$data['post_author'] = $this->mapping['user_slug'][ $author ];
+		}
+		else {
+			$meta[] = array( 'key' => '_wxr_import_user_slug', 'value' => $author );
+			$requires_remapping = true;
+
+			$data['post_author'] = (int) get_current_user_id();
+		}
+
+		// Does the post look like it contains attachment images?
+		if ( preg_match( self::REGEX_HAS_ATTACHMENT_REFS, $data['post_content'] ) ) {
+			$meta[] = array( 'key' => '_wxr_import_has_attachment_refs', 'value' => true );
+			$requires_remapping = true;
+		}
+
+		// Whitelist to just the keys we allow
+		$postdata = array(
+			'import_id' => $data['post_id'],
+		);
+		$allowed = array(
+			'post_author'    => true,
+			'post_date'      => true,
+			'post_date_gmt'  => true,
+			'post_content'   => true,
+			'post_excerpt'   => true,
+			'post_title'     => true,
+			'post_status'    => true,
+			'post_name'      => true,
+			'comment_status' => true,
+			'ping_status'    => true,
+			'guid'           => true,
+			'post_parent'    => true,
+			'menu_order'     => true,
+			'post_type'      => true,
+			'post_password'  => true,
+		);
+		foreach ( $data as $key => $value ) {
+			if ( ! isset( $allowed[ $key ] ) ) {
+				continue;
+			}
+
+			$postdata[ $key ] = $data[ $key ];
+		}
+
+		$postdata = apply_filters( 'wp_import_post_data_processed', $postdata, $data );
+
+		if ( 'attachment' === $postdata['post_type'] ) {
+			if ( ! $this->options['fetch_attachments'] ) {
+				$this->logger->notice( sprintf(
+					__( 'Skipping attachment "%s", fetching attachments disabled' ),
+					$data['post_title']
+				) );
+				return false;
+			}
+			$remote_url = ! empty($data['attachment_url']) ? $data['attachment_url'] : $data['guid'];
+			$post_id = $this->process_attachment( $postdata, $meta, $remote_url );
+		} else {
+			$post_id = wp_insert_post( $postdata, true );
+			do_action( 'wp_import_insert_post', $post_id, $original_id, $postdata, $data );
+		}
+
+		if ( is_wp_error( $post_id ) ) {
+			$this->logger->error( sprintf(
+				__( 'Failed to import "%s" (%s)', 'wordpress-importer' ),
+				$data['post_title'],
+				$post_type_object->labels->singular_name
+			) );
+			$this->logger->debug( $post_id->get_error_message() );
+
+			/**
+			 * Post processing failed.
+			 *
+			 * @param WP_Error $post_id Error object.
+			 * @param array $data Raw data imported for the post.
+			 * @param array $meta Raw meta data, already processed by {@see process_post_meta}.
+			 * @param array $comments Raw comment data, already processed by {@see process_comments}.
+			 * @param array $terms Raw term data, already processed.
+			 */
+			do_action( 'wxr_importer.process_failed.post', $post_id, $data, $meta, $comments, $terms );
+			return false;
+		}
+
+		// Ensure stickiness is handled correctly too
+		if ( $data['is_sticky'] == 1 ) {
+			stick_post( $post_id );
+		}
+
+		// map pre-import ID to local ID
+		$this->mapping['post'][ $original_id ] = (int) $post_id;
+		if ( $requires_remapping ) {
+			$this->requires_remapping['post'][ $post_id ] = true;
+		}
+		$this->mark_post_exists( $data, $post_id );
+
+		$this->logger->info( sprintf(
+			__( 'Imported "%s" (%s)', 'wordpress-importer' ),
+			$data['post_title'],
+			$post_type_object->labels->singular_name
+		) );
+		$this->logger->debug( sprintf(
+			__( 'Post %d remapped to %d', 'wordpress-importer' ),
+			$original_id,
+			$post_id
+		) );
+
+		// Handle the terms too
+		$terms = apply_filters( 'wp_import_post_terms', $terms, $post_id, $data );
+
+		if ( ! empty( $terms ) ) {
+			$term_ids = array();
+			foreach ( $terms as $term ) {
+				$taxonomy = $term['taxonomy'];
+				$key = sha1( $taxonomy . ':' . $term['slug'] );
+
+				if ( isset( $this->mapping['term'][ $key ] ) ) {
+					// !!!OCDI: The only change from the original importer code is this typecasting (int) bellow.
+					// !!!OCDI: The only change from the original importer code is this typecasting (int) bellow.
+					// !!!OCDI: The only change from the original importer code is this typecasting (int) bellow.
+					$term_ids[ $taxonomy ][] = (int) $this->mapping['term'][ $key ];
+				}
+				else {
+					$meta[] = array( 'key' => '_wxr_import_term', 'value' => $term );
+					$requires_remapping = true;
+				}
+			}
+
+			foreach ( $term_ids as $tax => $ids ) {
+				$tt_ids = wp_set_post_terms( $post_id, $ids, $tax );
+				do_action( 'wp_import_set_post_terms', $tt_ids, $ids, $tax, $post_id, $data );
+			}
+		}
+
+		$this->process_comments( $comments, $post_id, $data );
+		$this->process_post_meta( $meta, $post_id, $data );
+
+		if ( 'nav_menu_item' === $data['post_type'] ) {
+			$this->process_menu_item_meta( $post_id, $data, $meta );
+		}
+
+		/**
+		 * Post processing completed.
+		 *
+		 * @param int $post_id New post ID.
+		 * @param array $data Raw data imported for the post.
+		 * @param array $meta Raw meta data, already processed by {@see process_post_meta}.
+		 * @param array $comments Raw comment data, already processed by {@see process_comments}.
+		 * @param array $terms Raw term data, already processed.
+		 */
+		do_action( 'wxr_importer.processed.post', $post_id, $data, $meta, $comments, $terms );
 	}
 }
