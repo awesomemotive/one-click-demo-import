505fb8b6da057a85fdc91bcc772306cad460f3cd
Gregor Capuder <capuderg@gmail.com>
2016-07-09 19:10:29 +0200

    Updated translation files.

--- one-click-demo-import.php
+++ one-click-demo-import.php
@@ -26,7 +26,7 @@ if ( version_compare( phpversion(), '5.3.2', '<' ) ) {
 	 * Hook it to the 'admin_notices' action.
 	 */
 	function ocdi_old_php_admin_error_notice() {
-		$message = sprintf( esc_html__( 'The %2$sOne Click Demo Import%3$s plugin requires %2$sPHP 5.3.2+%3$s to run properly. Please contact your hosting company and ask them to update the PHP version of your site to at least PHP 5.3.2.%4$s Your current version of PHP: %2$s%1$s%3$s', 'structurepress-pt' ), phpversion(), '<strong>', '</strong>', '<br>' );
+		$message = sprintf( esc_html__( 'The %2$sOne Click Demo Import%3$s plugin requires %2$sPHP 5.3.2+%3$s to run properly. Please contact your hosting company and ask them to update the PHP version of your site to at least PHP 5.3.2.%4$s Your current version of PHP: %2$s%1$s%3$s', 'pt-ocdi' ), phpversion(), '<strong>', '</strong>', '<br>' );
 
 		printf( '<div class="notice notice-error"><p>%1$s</p></div>', wp_kses_post( $message ) );
 	}
