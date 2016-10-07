b2e17dbca2e9c7145eadbede83ae70aeeece5752
Gregor Capuder <capuderg@gmail.com>
2016-10-07 09:39:32 +0200

    Move content import code to the Importer class

--- inc/Importer.php
+++ inc/Importer.php
@@ -15,6 +15,33 @@ class Importer {
 	 */
 	private $importer;
 
+	/**
+	 * Time in milliseconds, marking the beginning of the import.
+	 *
+	 * @var float
+	 */
+	private $microtime;
+
+	/**
+	 * The instance of the OCDI\Logger class.
+	 *
+	 * @var object
+	 */
+	private $logger;
+
+	/**
+	 * The instance of the One Click Demo Import class.
+	 *
+	 * @var object
+	 */
+	private $ocdi;
+
+	/**
+	 * Constructor method.
+	 *
+	 * @param array  $importer_options Importer options.
+	 * @param object $logger           Logger object used in the importer.
+	 */
 	public function __construct( $importer_options = array(), $logger = null ) {
 		// Include files that are needed for WordPress Importer v2.
 		$this->include_required_files();
@@ -24,10 +51,15 @@ class Importer {
 		$this->importer = new WXRImporter( $importer_options );
 
 		// Set logger to the importer.
-		if ( ! empty( $logger ) ) {
-			$this->set_logger( $logger );
+		$this->logger = $logger;
+		if ( ! empty( $this->logger ) ) {
+			$this->set_logger( $this->logger );
 		}
+
+		// Get the OCDI (main plugin class) instance.
+		$this->ocdi = OneClickDemoImport::get_instance();
 	}
+
 
 	/**
 	 * Include required files.
@@ -39,6 +71,7 @@ class Importer {
 		}
 	}
 
+
 	/**
 	 * Imports content from a WordPress export file.
 	 *
@@ -47,6 +80,7 @@ class Importer {
 	public function import( $data_file ) {
 		$this->importer->import( $data_file );
 	}
+
 
 	/**
 	 * Set the logger used in the import
@@ -57,12 +91,14 @@ class Importer {
 		$this->importer->set_logger( $logger );
 	}
 
+
 	/**
 	 * Get all protected variables from the WXR_Importer needed for continuing the import.
 	 */
 	public function get_importer_data() {
 		return $this->importer->get_importer_data();
 	}
+
 
 	/**
 	 * Sets all protected variables from the WXR_Importer needed for continuing the import.
@@ -71,5 +107,92 @@ class Importer {
 	 */
 	public function set_importer_data( $data ) {
 		$this->importer->set_importer_data( $data );
+	}
+
+
+	/**
+	 * Import content from an WP XML file.
+	 *
+	 * @param string $import_file_path Path to the import file.
+	 */
+	public function import_content( $import_file_path ) {
+		$this->microtime = microtime( true );
+
+		// Increase PHP max execution time. Just in case, even though the AJAX calls are only 25 sec long.
+		set_time_limit( apply_filters( 'pt-ocdi/set_time_limit_for_demo_data_import', 300 ) );
+
+		// Disable import of authors.
+		add_filter( 'wxr_importer.pre_process.user', '__return_false' );
+
+		// Check, if we need to send another AJAX request and set the importing author to the current user.
+		add_filter( 'wxr_importer.pre_process.post', array( $this, 'new_ajax_request_maybe' ) );
+
+		// Disables generation of multiple image sizes (thumbnails) in the content import step.
+		if ( ! apply_filters( 'pt-ocdi/regenerate_thumbnails_in_content_import', true ) ) {
+			add_filter( 'intermediate_image_sizes_advanced', '__return_null' );
+		}
+
+		// Import content.
+		if ( ! empty( $import_file_path ) ) {
+			ob_start();
+				$this->import( $import_file_path );
+			$message = ob_get_clean();
+		}
+
+		// Delete content importer data for current import from DB.
+		delete_transient( 'ocdi_importer_data' );
+
+		// Return any error messages for the front page output (errors, critical, alert and emergency level messages only).
+		return $this->logger->error_output;
+	}
+
+
+	/**
+	 * Check if we need to create a new AJAX request, so that server does not timeout.
+	 *
+	 * @param array $data current post data.
+	 * @return array
+	 */
+	public function new_ajax_request_maybe( $data ) {
+		$time = microtime( true ) - $this->microtime;
+
+		// We should make a new ajax call, if the time is right.
+		if ( $time > apply_filters( 'pt-ocdi/time_for_one_ajax_call', 25 ) ) {
+			$this->set_current_importer_data();
+
+			$response = array(
+				'status'  => 'newAJAX',
+				'message' => 'Time for new AJAX request!: ' . $time,
+			);
+
+			// Add any output to the log file and clear the buffers.
+			$message = ob_get_clean();
+
+			// Add message to log file.
+			$log_added = Helpers::append_to_file(
+				__( 'New AJAX call!' , 'pt-ocdi' ) . PHP_EOL . $message,
+				$this->ocdi->get_log_file_path(),
+				''
+			);
+
+			wp_send_json( $response );
+		}
+
+		// Set importing author to the current user.
+		// Fixes the [WARNING] Could not find the author for ... log warning messages.
+		$current_user_obj    = wp_get_current_user();
+		$data['post_author'] = $current_user_obj->user_login;
+
+		return $data;
+	}
+
+
+	/**
+	 * Set current state of the content importer, so we can continue the import with new AJAX request.
+	 */
+	private function set_current_importer_data() {
+		$data = array_merge( $this->ocdi->get_current_importer_data(), $this->get_importer_data() );
+
+		set_transient( 'ocdi_importer_data', $data, 0.5 * HOUR_IN_SECONDS );
 	}
 }
--- inc/OneClickDemoImport.php
+++ inc/OneClickDemoImport.php
@@ -40,14 +40,7 @@ class OneClickDemoImport {
 	private $import_files;
 
 	/**
-	 * The instance of the OCDI\Logger class.
-	 *
-	 * @var object
-	 */
-	private $logger;
-
-	/**
-	 * The path of the log file
+	 * The path of the log file.
 	 *
 	 * @var string
 	 */
@@ -68,25 +61,11 @@ class OneClickDemoImport {
 	private $selected_import_files;
 
 	/**
-	 * Time in milliseconds, marking the beginning of the import.
-	 *
-	 * @var float
-	 */
-	private $microtime;
-
-	/**
 	 * Holds any error messages, that should be printed out at the end of the import.
 	 *
 	 * @var string
 	 */
 	private $frontend_error_messages;
-
-	/**
-	 * A counter of how many AJAX calls have been made.
-	 *
-	 * @var int
-	 */
-	private $ajax_call_number;
 
 
 	/**
@@ -210,12 +189,9 @@ class OneClickDemoImport {
 		Helpers::verify_ajax_call();
 
 		// Is this a new AJAX call to continue the previous import?
-		$use_existing_importer_data = $this->get_importer_data();
+		$use_existing_importer_data = $this->set_existing_importer_data();
 
 		if ( ! $use_existing_importer_data ) {
-			// Set the AJAX call number.
-			$this->ajax_call_number = empty( $this->ajax_call_number ) ? 0 : $this->ajax_call_number;
-
 			// Error messages displayed on front page.
 			$this->frontend_error_messages = '';
 
@@ -277,7 +253,7 @@ class OneClickDemoImport {
 		 * 2. Import content.
 		 * Returns any errors greater then the "error" logger level, that will be displayed on front page.
 		 */
-		$this->frontend_error_messages .= $this->import_content( $this->selected_import_files['content'] );
+		$this->frontend_error_messages .= $this->importer->import_content( $this->selected_import_files['content'] );
 
 		/**
 		 * 3. Before widgets import setup.
@@ -337,58 +313,6 @@ class OneClickDemoImport {
 		}
 
 		wp_send_json( $response );
-	}
-
-
-	/**
-	 * Import content from an WP XML file.
-	 *
-	 * @param string $import_file_path path to the import file.
-	 */
-	private function import_content( $import_file_path ) {
-		$this->microtime = microtime( true );
-
-		// This should be replaced with multiple AJAX calls (import in smaller chunks)
-		// so that it would not come to the Internal Error, because of the PHP script timeout.
-		// Also this function has no effect when PHP is running in safe mode
-		// http://php.net/manual/en/function.set-time-limit.php.
-		// Increase PHP max execution time.
-		set_time_limit( apply_filters( 'pt-ocdi/set_time_limit_for_demo_data_import', 300 ) );
-
-		// Disable import of authors.
-		add_filter( 'wxr_importer.pre_process.user', '__return_false' );
-
-		// Check, if we need to send another AJAX request and set the importing author to the current user.
-		add_filter( 'wxr_importer.pre_process.post', array( $this, 'new_ajax_request_maybe' ) );
-
-		// Disables generation of multiple image sizes (thumbnails) in the content import step.
-		if ( ! apply_filters( 'pt-ocdi/regenerate_thumbnails_in_content_import', true ) ) {
-			add_filter( 'intermediate_image_sizes_advanced',
-				function() {
-					return null;
-				}
-			);
-		}
-
-		// Import content.
-		if ( ! empty( $import_file_path ) ) {
-			ob_start();
-				$this->importer->import( $import_file_path );
-			$message = ob_get_clean();
-
-			// Add this message to log file.
-			$log_added = Helpers::append_to_file(
-				$message . PHP_EOL . esc_html__( 'Max execution time after content import = ' , 'pt-ocdi' ) . ini_get( 'max_execution_time' ),
-				$this->log_file_path,
-				esc_html__( 'Importing content' , 'pt-ocdi' )
-			);
-		}
-
-		// Delete content importer data for current import from DB.
-		delete_transient( 'ocdi_importer_data' );
-
-		// Return any error messages for the front page output (errors, critical, alert and emergency level messages only).
-		return $this->logger->error_output;
 	}
 
 
@@ -482,78 +406,48 @@ class OneClickDemoImport {
 
 
 	/**
-	 * Check if we need to create a new AJAX request, so that server does not timeout.
-	 *
-	 * @param array $data current post data.
-	 * @return array
-	 */
-	public function new_ajax_request_maybe( $data ) {
-		$time = microtime( true ) - $this->microtime;
-
-		// We should make a new ajax call, if the time is right.
-		if ( $time > apply_filters( 'pt-ocdi/time_for_one_ajax_call', 25 ) ) {
-			$this->ajax_call_number++;
-			$this->set_importer_data();
-
-			$response = array(
-				'status'  => 'newAJAX',
-				'message' => 'Time for new AJAX request!: ' . $time,
-			);
-
-			// Add any output to the log file and clear the buffers.
-			$message = ob_get_clean();
-
-			// Add message to log file.
-			$log_added = Helpers::append_to_file(
-				__( 'Completed AJAX call number: ' , 'pt-ocdi' ) . $this->ajax_call_number . PHP_EOL . $message,
-				$this->log_file_path,
-				''
-			);
-
-			wp_send_json( $response );
-		}
-
-		// Set importing author to the current user.
-		// Fixes the [WARNING] Could not find the author for ... log warning messages.
-		$current_user_obj    = wp_get_current_user();
-		$data['post_author'] = $current_user_obj->user_login;
-
-		return $data;
-	}
-
-	/**
-	 * Set current state of the content importer, so we can continue the import with new AJAX request.
-	 */
-	private function set_importer_data() {
-		$data = array(
-			'frontend_error_messages' => $this->frontend_error_messages,
-			'ajax_call_number'        => $this->ajax_call_number,
-			'log_file_path'           => $this->log_file_path,
-			'selected_index'          => $this->selected_index,
-			'selected_import_files'   => $this->selected_import_files,
-		);
-
-		$data = array_merge( $data, $this->importer->get_importer_data() );
-
-		set_transient( 'ocdi_importer_data', $data, 0.5 * HOUR_IN_SECONDS );
-	}
-
-	/**
 	 * Get content importer data, so we can continue the import with this new AJAX request.
+	 *
+	 * @return boolean
 	 */
-	private function get_importer_data() {
+	private function set_existing_importer_data() {
 		if ( $data = get_transient( 'ocdi_importer_data' ) ) {
-			$this->frontend_error_messages                = empty( $data['frontend_error_messages'] ) ? '' : $data['frontend_error_messages'];
-			$this->ajax_call_number                       = empty( $data['ajax_call_number'] ) ? 1 : $data['ajax_call_number'];
-			$this->log_file_path                          = empty( $data['log_file_path'] ) ? '' : $data['log_file_path'];
-			$this->selected_index                         = empty( $data['selected_index'] ) ? 0 : $data['selected_index'];
-			$this->selected_import_files                  = empty( $data['selected_import_files'] ) ? array() : $data['selected_import_files'];
+			$this->frontend_error_messages = empty( $data['frontend_error_messages'] ) ? '' : $data['frontend_error_messages'];
+			$this->log_file_path           = empty( $data['log_file_path'] ) ? '' : $data['log_file_path'];
+			$this->selected_index          = empty( $data['selected_index'] ) ? 0 : $data['selected_index'];
+			$this->selected_import_files   = empty( $data['selected_import_files'] ) ? array() : $data['selected_import_files'];
 			$this->importer->set_importer_data( $data );
 
 			return true;
 		}
 		return false;
 	}
+
+
+	/**
+	 * Get the current state of selected data.
+	 *
+	 * @return array
+	 */
+	public function get_current_importer_data() {
+		return array(
+			'frontend_error_messages' => $this->frontend_error_messages,
+			'log_file_path'           => $this->log_file_path,
+			'selected_index'          => $this->selected_index,
+			'selected_import_files'   => $this->selected_import_files,
+		);
+	}
+
+
+	/**
+	 * Getter function to retrieve the private log_file_path value.
+	 *
+	 * @return string The log_file_path value.
+	 */
+	public function get_log_file_path() {
+		return $this->log_file_path;
+	}
+
 
 	/**
 	 * Load the plugin textdomain, so that translations can be made.
@@ -581,10 +475,10 @@ class OneClickDemoImport {
 		) );
 
 		// Configure logger instance and set it to the importer.
-		$this->logger            = new Logger();
-		$this->logger->min_level = $logger_options['logger_min_level'];
+		$logger            = new Logger();
+		$logger->min_level = $logger_options['logger_min_level'];
 
 		// Create importer instance with proper parameters.
-		$this->importer = new Importer( $importer_options, $this->logger );
+		$this->importer = new Importer( $importer_options, $logger );
 	}
 }
