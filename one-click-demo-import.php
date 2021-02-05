bb6c8cf00254c782a9e7591e7b52ef85c70e4bb3
Gregor Capuder <capuderg@gmail.com>
2021-02-05 08:53:45 +0100

    Update minimal PHP version check to 5.6

--- one-click-demo-import.php
+++ one-click-demo-import.php
@@ -24,10 +24,10 @@ class OCDI_Plugin {
 	 */
 	public function __construct() {
 		/**
-		 * Display admin error message if PHP version is older than 5.3.2.
+		 * Display admin error message if PHP version is older than 5.6.
 		 * Otherwise execute the main plugin class.
 		 */
-		if ( version_compare( phpversion(), '5.3.2', '<' ) ) {
+		if ( version_compare( phpversion(), '5.6', '<' ) ) {
 			add_action( 'admin_notices', array( $this, 'old_php_admin_error_notice' ) );
 		}
 		else {
@@ -50,11 +50,11 @@ class OCDI_Plugin {
 
 
 	/**
-	 * Display an admin error notice when PHP is older the version 5.3.2.
+	 * Display an admin error notice when PHP is older the version 5.6.
 	 * Hook it to the 'admin_notices' action.
 	 */
 	public function old_php_admin_error_notice() {
-		$message = sprintf( esc_html__( 'The %2$sOne Click Demo Import%3$s plugin requires %2$sPHP 5.3.2+%3$s to run properly. Please contact your hosting company and ask them to update the PHP version of your site to at least PHP 5.3.2.%4$s Your current version of PHP: %2$s%1$s%3$s', 'pt-ocdi' ), phpversion(), '<strong>', '</strong>', '<br>' );
+		$message = sprintf( esc_html__( 'The %2$sOne Click Demo Import%3$s plugin requires %2$sPHP 5.6+%3$s to run properly. Please contact your hosting company and ask them to update the PHP version of your site to at least PHP 7.4%4$s Your current version of PHP: %2$s%1$s%3$s', 'pt-ocdi' ), phpversion(), '<strong>', '</strong>', '<br>' );
 
 		printf( '<div class="notice notice-error"><p>%1$s</p></div>', wp_kses_post( $message ) );
 	}
