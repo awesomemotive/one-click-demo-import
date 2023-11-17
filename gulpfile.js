c3020101569f43c06c4b524c23ff48e806f5be83
Michael Joseph Panaga <donmhico@gmail.com>
2023-11-17 17:37:09 +0800

    Add replace_ver task in build task

--- gulpfile.js
+++ gulpfile.js
@@ -157,6 +157,24 @@ gulp.task( 'copy', function () {
 		.pipe( debug( { title: '[copy]' } ) );
 } );
 
+/**
+ * Replace plugin version with one from package.json in the main plugin file.
+ */
+gulp.task( 'replace_plugin_file_ver', function () {
+	return gulp.src( [ 'one-click-demo-import.php' ] )
+		.pipe(
+			// File header.
+			replace(
+				/Version: ((\*)|([0-9]+(\.((\*)|([0-9]+(\.((\*)|([0-9]+)))?)))?))/gm,
+				'Version: ' + packageJSON.version
+			)
+		)
+		.pipe( gulp.dest( './' ) );
+} );
+
+/**
+ * Replace plugin version with one from package.json in @since comments in plugin PHP and JS files.
+ */
 gulp.task( 'replace_since_ver', function() {
 	return gulp.src( plugin.files_replace_ver )
 		.pipe(
@@ -168,10 +186,12 @@ gulp.task( 'replace_since_ver', function() {
 		.pipe( gulp.dest( './' ) );
 } );
 
+gulp.task( 'replace_ver', gulp.series( 'replace_plugin_file_ver', 'replace_since_ver' ) );
+
 /**
  * Task: build.
  */
-gulp.task( 'build', gulp.series( gulp.parallel( 'css', 'js', 'pot' ), 'copy' ) );
+gulp.task( 'build', gulp.series( gulp.parallel( 'css', 'js', 'pot' ), 'replace_ver', 'copy' ) );
 
 /**
  * Look out for relevant sass/js changes.
